e9490939a5fd33c9e57a3052dc598479b8fefb85
Angela Collings <angelacollings@developersinstitute.co.nz>
2023-07-17 07:27:10 +1200

    create home component with bio, button and hero image

---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import HeroImage from "../assets/photo.png";
+import { SlArrowRight } from "react-icons/sl";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-gray-900 to bg-gray-900"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full max-w-lg">
+          <p className="text-gray-400">Hi, my name is</p>
+          <h2 className="text-5xl sm:text-7xl font-bold text-yellow-700 py-3">
+            Angela Collings.
+          </h2>
+          <h3 className="text-2xl sm:text-4xl font-bold text-gray-400">
+            I'm a Software Developer.
+          </h3>
+          <p className=" text-gray-400 text-lg py-8 ">
+            I have a passion for problem solving and creating things. Currently
+            I am enjoying learning how to navigate Wordpress and working on a
+            few personal projects to expand my skill-set in React and Node.js.
+          </p>
+          <div>
+            <button className="group text-black w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-yellow-700 to-yellow-500 cursor-pointer">
+              View Projects
+              <span className="group-hover:rotate-[450deg] duration-500 ml-4">
+                <SlArrowRight size={15} />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img src={HeroImage} alt="hero" className="mx-auto w-2/3 md:2/3" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
